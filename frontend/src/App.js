fc781830ff3a703edce473be142fad5cc3dcf62f
intcontrol <katumbuernest@gmail.com>
2026-05-10 13:20:59 +0100

    Add project structure and basic setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SafeStock</h1>
+        <p>Application de gestion de stock</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
